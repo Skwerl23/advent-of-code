65dc2c67428c7be9f0fe829cffb27c98045baac6
Skwerl23 <cottermannelson@gmail.com>
2022-12-10 11:13:36 -0700

    severly reduced the code

--- challenge05/challenge5.ps1
+++ challenge05/challenge5.ps1
@@ -1,38 +1,20 @@
-﻿$rules = cat C:\Tools\advent2022\challenge5.txt
+$rules = cat C:\Tools\advent2022\challenge5.txt
 
 $stacks = $rules[0..8]
 $moves = $rules[10..10000]
  
-$a1=@()
-$a2=@()
-$a3=@()
-$a4=@()
-$a5=@()
-$a6=@()
-$a7=@()
-$a8=@()
-$a9=@()
-foreach($x in 0..7) {
-        $a1+=$stacks[$x][1]
-        $a2+=$stacks[$x][5]
-        $a3+=$stacks[$x][9]
-        $a4+=$stacks[$x][13]
-        $a5+=$stacks[$x][17]
-        $a6+=$stacks[$x][21]
-        $a7+=$stacks[$x][25]
-        $a8+=$stacks[$x][29]
-        $a9+=$stacks[$x][33]
+foreach ($x in 1..9) {
+    Set-Variable a$x -Value @()
 }
-$a1 = $a1[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a2 = $a2[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a3 = $a3[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a4 = $a4[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a5 = $a5[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a6 = $a6[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a7 = $a7[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a8 = $a8[-1..-1000] | where {$_ -match '[a-zA-Z]'}
-$a9 = $a9[-1..-1000] | where {$_ -match '[a-zA-Z]'}
 
+foreach($x in 0..7) {
+    foreach ($i in 0..8) {
+        Set-Variable "a$($i+1)" -Value ((Get-Variable "a$($i+1)" -ValueOnly) + $stacks[$x][($i*4)+1])
+    }
+}
+$answer = foreach ($x in 1..9) {
+    Set-Variable a$x ((get-Variable a$x -ValueOnly)[-1..-1000] | where {$_ -match '[a-zA-Z]'})   
+}
 
 foreach ($m in $moves) {
     $m = $m.split(' ')
@@ -45,14 +27,7 @@ foreach ($m in $moves) {
     $x = $x[-1000..((-$m[1])-1)]
     Set-Variable "a$($m[3])" -Value $x
 }
-
-
-$a1[-1]
-$a2[-1]
-$a3[-1]
-$a4[-1]
-$a5[-1]
-$a6[-1]
-$a7[-1]
-$a8[-1]
-$a9[-1]
+$answer = foreach ($x in 1..9) {
+    (get-Variable a$x -ValueOnly)[-1]
+}
+$answer -join ''
