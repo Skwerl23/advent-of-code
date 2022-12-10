550d7b30a61140eba653ba18718c7476e6fa4994
Skwerl23 <cottermannelson@gmail.com>
2022-12-10 00:01:19 -0700

    Add files via upload

---
+++ challenge08 part2.ps1
@@ -0,0 +1,73 @@
+﻿$challenge8 = Get-Content "C:\Tools\advent2022\challenge8.txt"
+
+$count=0
+$max = 0
+$ErrorActionPreference = "silentlycontinue"
+foreach ($i in 0..98) {
+
+    foreach ($j in 0..98) {
+
+        $left = 1
+        $top = 1
+        $right = 1
+        $bottom = 1
+        foreach ($a in ($i-1)..0) {
+            if ($challenge8[$a][$j] -ge $challenge8[$i][$j]) {
+                $top = [math]::max(1,[int]$i - $a)
+                break
+            }
+            elseif ($a -eq 0) {
+                $top = [math]::max(1,[int]$i - $a)
+                break
+            }
+
+        }
+        foreach ($a in ($i+1)..98) {
+            if ($challenge8[$a][$j] -ge $challenge8[$i][$j]) {
+                $bottom = [math]::max(1,[int]$a - $i)
+                break
+            }
+            elseif ($a -eq 98) {
+                $bottom = [math]::max(1,[int]$a - $i)                
+        
+                break
+
+            }
+
+        }
+        foreach ($a in ($j-1)..0) {
+            if ($challenge8[$i][$a] -ge $challenge8[$i][$j]) {
+                $left = [math]::max(1,[int]$j - $a)
+                break
+            }
+            elseif ($a -eq 0) {
+                $left = [math]::max(1,[int]$j - $a)
+                break
+
+            }
+        }
+        foreach ($a in ($j+1)..98) {
+            if ($challenge8[$i][$a] -ge $challenge8[$i][$j]) {
+                $right = [math]::max(1,[int]$a - $j)
+                break
+            }
+            elseif ($a -eq 98) {
+                $right = [math]::max(1,[int]$a - $j)         
+                break
+            }
+        }
+            $x = write-output $max
+            $max = [math]::Max($max, $left*$right*$top*$bottom)
+            if ($max -ne $x) {
+                write-output "$i , $j, $($challenge8[$i][$j])"
+                
+                write-output "$left, $right, $top, $bottom"
+                $max
+            }
+
+
+        
+    }
+   
+}
+$max
---
+++ challenge08.ps1
@@ -0,0 +1,38 @@
+﻿$input = Get-Content "C:\Tools\advent2022\challenge8.txt"
+$count=0
+foreach ($i in 0..98) {
+    foreach ($j in 0..98) {
+        $left = $true
+        $top = $true
+        $right = $true
+        $bottom = $true
+        foreach ($a in 0..($i-1)) {
+            if ($input[$a][$j] -ge $input[$i][$j]) {
+                $left = $false
+            }
+        }
+        foreach ($a in ($i+1)..98) {
+            if ($input[$a][$j] -ge $input[$i][$j]) {
+                $right = $false
+            }
+        }
+        foreach ($a in 0..($j-1)) {
+            if ($input[$i][$a] -ge $input[$i][$j]) {
+                $top = $false
+            }
+        }
+        foreach ($a in ($j+1)..98) {
+            if ($input[$i][$a] -ge $input[$i][$j]) {
+                $bottom = $false
+            }
+        }
+        if ($j -eq 0 -or $j -eq 98 -or $i -eq 0 -or $i -eq 98) {
+            $left = $true
+        }
+        if ($left -or $right -or $top -or $bottom) {
+            $count ++
+        }
+
+    }
+}
+$count
