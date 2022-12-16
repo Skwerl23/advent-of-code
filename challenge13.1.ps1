123591d0f5b60287fcf606939f5cf8ac5ec77d33
Skwerl23 <cottermannelson@gmail.com>
2022-12-15 21:42:08 -0700

    Add files via upload

---
+++ challenge13.1.ps1
@@ -0,0 +1,41 @@
+﻿$data = Get-Content C:\Tools\advent2022\challenge13.txt
+
+function Compare-Arrays($left, $right) {
+    $leftParts = $left.Split(",")
+    $rightParts = $right.Split(",")
+    for ($i = 0; $i -lt $leftParts.Length; $i++) {
+        $l = $leftParts[$i]
+        $r = $rightParts[$i]
+
+        $dl = ($l.length - $l.replace("[",'').length) - ($l.length - $l.replace("]",'').length)
+        $dr = ($r.length - $r.replace("[",'').length) - ($r.length - $r.replace("]",'').length)
+
+        $l = $l.Trim("[]")
+        $r = $r.Trim("[]")
+
+        if ($l -ne $r) {
+            if ($l -and $r) {
+                return [int]$l - [int]$r
+            }
+            return ([bool]$l) - ([bool]$r)
+        }
+        if ($dl -ne $dr) {
+            return $dl - $dr
+        }
+    }
+    return $left.Length - $right.Length
+}
+
+$count = 0
+$final = 0
+for ($x=0; $x -lt ($data.Length-1);$x+=3) {
+$count ++
+    $left = $data[$x]
+    $right = $data[$x+1]
+    
+    if ((compare-arrays $left $right) -lt 0) {
+        $final += $count
+    }
+}
+
+$final
---
+++ challenge13.2.ps1
@@ -0,0 +1,57 @@
+﻿$data = Get-Content C:\Tools\advent2022\challenge13.txt
+
+function Compare-Arrays($left, $right) {
+    $leftParts = $left.Split(",")
+    $rightParts = $right.Split(",")
+
+    for ($i = 0; $i -lt $leftParts.Length; $i++) {
+        $l = $leftParts[$i]
+        $r = $rightParts[$i]
+
+        $dl = ($l.length - $l.replace("[",'').length) - ($l.length - $l.replace("]",'').length)
+        $dr = ($r.length - $r.replace("[",'').length) - ($r.length - $r.replace("]",'').length)
+
+        $l = $l.Trim("[]")
+        $r = $r.Trim("[]")
+
+        if ($l -ne $r) {
+            if ($l -and $r) {
+                return [int]$l - [int]$r
+            }
+            return ([bool]$l) - ([bool]$r)
+        }
+        if ($dl -ne $dr) {
+            return $dl - $dr
+        }
+    }
+    return $left.Length - $right.Length
+}
+
+$add1 = "[[2]]"
+$data += $add1
+$add2 = "[[6]]"
+$data += $add2
+$data = $data | where {$_ -match '.'}
+
+$final = 0
+$notSorted = $true
+while ($notSorted) {
+    $notsorted = $false
+    for ($i = 0; $i -lt $data.Length-1; $i++) {
+        $left = $data[$i]
+        $right = $data[$i+1]
+        if ((compare-arrays $left $right) -ge 1) {
+            $swap = $left.Clone()
+            $data[$i] = $right.Clone()
+            $data[$i+1] = $swap
+            $notSorted=$true
+        }
+
+    }
+    
+}
+
+$a = $data.IndexOf($add1)+1
+$b = $data.IndexOf($add2)+1
+"answer= " + ($a * $b)
+
