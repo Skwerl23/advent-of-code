b37e56db0ae037ebada6cf7c261e7a28ad27eacc
Skwerl23 <cottermannelson@gmail.com>
2023-01-12 00:51:52 -0700

    Completed challenge 21 in C#
    need to re-write powershell search algorithm to be binary search.

--- Challenge21/Challenge21.cs
+++ Challenge21/Challenge21.cs
@@ -1,14 +1,276 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Year22
 {
     public class Day21 {
+
+        private static ulong calculateValue (ulong left, ulong right, char sign) {
+                if (sign == '+') {
+                    return left+right;
+                }
+                else if (sign == '-') {
+                    return left-right;
+                }
+                else if (sign == '/') {
+                    return left/right;
+                }
+                else if (sign == '*') {
+                    return left*right;
+                }
+                else if (sign == '=') {
+                    if (left > right) {
+                        return 2;
+                    }
+                    if (left < right) {
+                        return 1;
+                    }
+                }
+                return 0;
+            }
+
+
         public static void Run () {
 Stopwatch stopwatch = new Stopwatch();
 stopwatch.Start();
-            Console.WriteLine("Nothing Written Yet");
+
+            //took around 26513 ms to run or approx 32x faster than Powershell
+            //this is using a different search algorithm.
+            List<string> data = File.ReadAllLines(@"C:\Tools\advent2022\Challenge21.txt").ToList();
+            int count=0;
+            string pattern = @"^root:\s\d+$";
+            string left;
+            string right;
+            string leftLong;
+            string rightLong;
+            string answer = "";
+            ulong tempLonga;
+            ulong tempLongb;
+            string sign;
+            bool moveOn;
+            string name;
+            string tempLeft;
+            string tempRight;
+            ulong result;
+            char tempCharA;
+            bool leftCheck;
+            bool rightCheck;
+            while( ! data.Exists(s => Regex.IsMatch(s, pattern)))
+                {
+                // increment count 
+                count++;
+                //initialize VARIABLES
+                left = "";
+                right = "";
+                leftLong = "blank";
+                rightLong = "blank";
+
+                //start loop to go through each value in the list of work
+                for (int i=0; i < data.Count; i++) {
+                    //reset values for left and right
+                    left = "";
+                    right = "";                    
+                    leftLong = "blank";
+                    rightLong = "blank";
+
+                    // determine if we have a sign
+                    if (data[i].Split(' ').Length > 2) {
+                        sign = data[i].Split(':')[1].Split(' ')[2];
+                    }
+                    else {sign = "";}
+                    //if there's a sign, we will work with the 2 values
+                    if (sign != "") {
+                        //the main name we're at is assigned, then the left and right values
+                        name = data[i].Split(':')[0];
+                        left = data[i].Split(':')[1].Split(' ')[1];
+                        right =data[i].Split(':')[1].Split(' ').Last();
+                        moveOn = true;
+                        tempLonga = 0;
+                        tempLongb = 0;
+                        //try to parse left and right values, if they are numbers, we calculate them
+                        leftCheck = ulong.TryParse(left, out tempLonga);
+                        rightCheck = ulong.TryParse(right, out tempLongb);
+                        if (leftCheck && rightCheck) {
+                            tempCharA = sign.ToCharArray()[0];
+                            
+                            answer = calculateValue(tempLonga, tempLongb, tempCharA).ToString();
+                            moveOn = false;
+                        }
+                        if (moveOn) {
+                            if (tempLonga == 0) {
+                                tempLeft = "^" + left + ":";
+                                tempLeft = data.Find(s => Regex.IsMatch(s, tempLeft))!;
+                                if (tempLeft.Split(' ').Length == 2) {
+                                    if ( ulong.TryParse(tempLeft.Split(' ')[1], out result) ) {
+                                        leftLong = result.ToString();
+                                    }
+                                }
+                            } else {leftLong = tempLonga.ToString();}
+                            if (tempLongb == 0) {
+                                tempRight = "^" + right + ":";
+                                tempRight = data.Find(s => Regex.IsMatch(s, tempRight))!;
+
+                                if (tempRight.Split(' ').Length == 2) {
+                                    if ( ulong.TryParse(tempRight.Split(' ')[1], out result) ) {
+                                        rightLong = result.ToString();
+                                    }
+                                }
+                            } else {rightLong = tempLongb.ToString();}
+                            if (leftLong != "blank" && rightLong != "blank") {
+                                answer = leftLong + " " + sign  + " " + rightLong;
+                            }
+                            else if (leftLong != "blank" && rightLong == "blank") {
+                                answer = leftLong + " " + sign + " " + right;
+                            }
+                            else if (leftLong == "blank" && rightLong != "blank") {
+                                answer = left + " " + sign + " " + rightLong;
+                            }
+                            else {
+                                answer = left + " " + sign + " " + right;
+                            }
+                        }
+                        data[i] = name + ": " + answer;
+                    }
+
+
+                }
+            }
+
+            string answer1 = data.Find(s => Regex.IsMatch(s, "root:"))!;
+            Console.WriteLine("Answer to 1 = " + answer1);
+
+            //Part 2 works so similarly, but requires retries, that i am restarting
+
+            data = File.ReadAllLines(@"C:\Tools\advent2022\Challenge21.txt").ToList();
+            count=0;
+            answer = "";
+            List<string> dataOld = new List<string> {""};
+            bool dataminimized = false;
+            //anything bigger than this number breaks c#
+            ulong humanMax = 10000000000000;
+            ulong humanMin = 0;
+            bool restart;
+            ulong testValue = 0;
+            while( ! data.Exists(s => Regex.IsMatch(s, pattern)))
+            {
+                //     Console.WriteLine();
+                // foreach (string line in data) {
+                //     Console.WriteLine(line);
+                // }
+                if (dataOld.SequenceEqual(data) && !dataminimized) {dataminimized = true;}
+                
+                if (data.Count(x => x.Contains("humn")) == 2 && !dataminimized) {
+                    dataOld = new List<string>(data);
+                }
+                restart = false;
+                // increment count 
+                count++;
+                //initialize VARIABLES
+                left = "";
+                right = "";
+                leftLong = "blank";
+                rightLong = "blank";
+                //start loop to go through each value in the list of work
+                testValue = ((humanMax + humanMin) / 2);
+                if (dataminimized) {
+//                    Console.WriteLine(testValue + " " + humanMin + " " + humanMax);
+                    int index = data.FindIndex(s => s.Contains("humn: "));
+                    data[index] = "humn: " + testValue.ToString();
+                }
+                for (int i=0; i < data.Count; i++) {
+                    //reset values for left and right
+                    left = "";
+                    right = "";                    
+                    leftLong = "blank";
+                    rightLong = "blank";
+                    if (!dataminimized) {
+                        if (data[i].Contains("humn")) {continue;}
+                    }
+                    // determine if we have a sign
+                    if (data[i].Split(' ').Length > 2) {
+                        sign = data[i].Split(':')[1].Split(' ')[2];
+                    }
+                    else {sign = "";}
+                    //if there's a sign, we will work with the 2 values
+                    if (sign != "") {
+                        //the main name we're at is assigned, then the left and right values
+                        name = data[i].Split(':')[0];
+                        left = data[i].Split(':')[1].Split(' ')[1];
+                        right =data[i].Split(':')[1].Split(' ').Last();
+                        moveOn = true;
+                        tempLonga = 0;
+                        tempLongb = 0;
+                        //try to parse left and right values, if they are numbers, we calculate them
+                        
+                        leftCheck = ulong.TryParse(left, out tempLonga);
+                        rightCheck = ulong.TryParse(right, out tempLongb);
+                        if (leftCheck && rightCheck && name != "root") {
+                            tempCharA = sign.ToCharArray()[0];
+                            
+                            answer = calculateValue(tempLonga, tempLongb, tempCharA).ToString();
+                            moveOn = false;
+                        }
+                        if (moveOn) {
+                            if (tempLonga == 0) {
+                                tempLeft = "^" + left + ":";
+                                tempLeft = data.Find(s => Regex.IsMatch(s, tempLeft))!;
+                                if (tempLeft.Split(' ').Length == 2) {
+                                    if ( ulong.TryParse(tempLeft.Split(' ')[1], out result) ) {
+                                        leftLong = result.ToString();
+                                    }
+                                }
+                            } else {leftLong = tempLonga.ToString();}
+                            if (tempLongb == 0) {
+                                tempRight = "^" + right + ":";
+                                tempRight = data.Find(s => Regex.IsMatch(s, tempRight))!;
+
+                                if (tempRight.Split(' ').Length == 2) {
+                                    if ( ulong.TryParse(tempRight.Split(' ')[1], out result) ) {
+                                        rightLong = result.ToString();
+                                    }
+                                }
+                            } else {rightLong = tempLongb.ToString();}
+                            if (leftLong != "blank" && rightLong != "blank") {
+                                if (name == "root" && calculateValue(ulong.Parse(leftLong), ulong.Parse(rightLong), '=') == 0) {
+                                    answer = testValue.ToString();
+                                }
+                                else if (name == "root" && calculateValue(ulong.Parse(leftLong), ulong.Parse(rightLong), '=') == 1) {
+                                    humanMax = testValue;
+                                    data = new List<string>(dataOld);
+                                    restart = true;
+                                }
+                                else if (name == "root" && calculateValue(ulong.Parse(leftLong), ulong.Parse(rightLong), '=') == 2) {
+                                    humanMin = testValue;
+                                    data = new List<string>(dataOld);
+                                    restart = true;
+                                }
+
+                                else {
+                                    answer = leftLong + " " + sign  + " " + rightLong;
+                                }
+
+                            }
+                            else if (leftLong != "blank" && rightLong == "blank") {
+                                answer = leftLong + " " + sign + " " + right;
+                            }
+                            else if (leftLong == "blank" && rightLong != "blank") {
+                                answer = left + " " + sign + " " + rightLong;
+                            }
+                            else {
+                                answer = left + " " + sign + " " + right;
+                            }
+                        }
+                        if (restart) {continue;}
+                        data[i] = name + ": " + answer;
+                    }
+
+
+                }
+            }
+            Console.WriteLine("Answer to 2 = " + answer);
+
 
 stopwatch.Stop();
 TimeSpan elapsed = stopwatch.Elapsed;
--- Challenge21/Challenge21.ps1
+++ Challenge21/Challenge21.ps1
@@ -1,24 +1,15 @@
-### There must be a more accurate way to do this, I sort of intelligently brute force it by going up and down one digit at a time until i hone in on the answer.
-### After looking at other solutions, people did a binary search. which is sort of like what i did. 
-#### Binary search goes by trying splitting the difference. so for example if the answer is 37 a binary search would do this:
-### start with 0 and 100. if 50 is too high, set 100 to 50. now try 25. it's too low, set 0 to 25. now try 37. (half way 25-50) it's too high. and so on
-### What i did was try most significant bit, if not high enough add 1. try again, if too high, subtract 1 and move down a tens place. for example
-### if 1000 is too low, try 2000. if 2000 is too high, try 2000-1000+100 or 1100. too low, try 1200..1300..1400 oh too high, now try 1310 etc...
-### obviously the binary search is faster.
-### out of a trillion we'd cut it to 500b then 250b then 125 b and so on. but thats 3 moves.
-### my method cuts 1trillion to 900b then 800b and has to try an average of 5 per place. so 70 tries on average, but could be 24 tries and could be 132 tries.
-###splitting in half... means it takes 40 tries every time no matter what
-### my method took approx 72 tries for my answer. if you have lower numbers say 3 digits it takes 10 tries, where my method takes 17.
-###so binary search seems to always be faster in terms of statistics. 
-### Maybe this doesn't work in all scenarios. You may need to reverse the check signs. for the root equality response
+﻿## My powershell version here uses incremental numbers, instead of a binary split search.
+## I will re-write this to support binary search, but for now it might be slower than expected
+
+# This took around xxx milliseconds
+
+
+Measure-Command {
+
 $data = Get-Content C:\Tools\advent2022\Challenge21.txt
-$data = $data.replace("root: wdzt + dffc","root: wdzt = dffc")
 
-$humanValue = ($data -match "humn:").split()[1]
 
-$humanCount = $data -match "humn" | measure |select -ExpandProperty count
-<#
-$data = @("root: pppw = sjmn"
+<#$data = @("root: pppw + sjmn"
 "dbpl: 5"
 "cczh: sllz + lgvd"
 "zczc: 2"
@@ -33,8 +24,7 @@ $data = @("root: pppw = sjmn"
 "lgvd: ljgn * ptdq"
 "drzm: hmdt - zczc"
 "hmdt: 32")
-#>                                  #####13439547545467
-
+#>
 function calculateValue ([int64]$left, [int64]$right, [string]$sign) {
     if ($sign -eq "+") {
         return $left + $right
@@ -60,6 +50,58 @@ function calculateValue ([int64]$left, [int64]$right, [string]$sign) {
         return 0
     }
 }
+$erroractionpreference = "SilentlyContinue"
+$count=0
+while (($data -match "root:").Split().length -gt 2){
+    $count++ 
+    for ($i=0; $i -lt $data.Length; $i++) {
+        $left = $right = $sign = $null
+        $sign = $data[$i].split(':')[1].split()[2]
+        if ($sign) {
+            $name = $data[$i].split(':')[0]
+            $left = $data[$i].split(':')[1].split()[1]
+            $right = $data[$i].split(':')[1].split()[-1]
+            $continue = $true
+            if ([int64]$right -is [int64] -and [int64]$left -is [int64]) {
+                $answer = calculateValue $left $right $sign
+                $continue = $false
+            }
+            if ($continue) {
+                $tempLeft = ($data -match ("^$left" + ":"))
+                if ($tempLeft.split().length -eq 2) {
+                    if ([int64]$templeft.split()[1]) {
+                        $left = $tempLeft.split()[1]
+                    }
+                }
+                $tempright = ($data -match ("^$right" + ":"))
+                if ($tempright.split().length -eq 2) {
+                    if ([int64]$tempright.split()[1]) {
+                        $right = $tempright.split()[1]
+                    }
+                }
+                $answer = $left + " $sign " + $right
+                
+            }
+            $data[$i] = "$name" + ": $answer"
+#            $data
+#            sleep 1
+        }
+
+
+    }    
+
+}
+
+$answer1 = $data -match "root:"
+write-host "Answer to 1 = $answer1"
+
+$data = Get-Content C:\Tools\advent2022\Challenge21.txt
+$data = $data.replace("root: wdzt + dffc","root: wdzt = dffc")
+
+$humanValue = ($data -match "humn:").split()[1]
+
+$humanCount = $data -match "humn" | measure |select -ExpandProperty count
+
 $erroractionpreference = "SilentlyContinue"
 $count=0
 ### my answer was around 3900000000000
@@ -144,5 +186,6 @@ while (($data -match "root:").Split().length -gt 2){
     }    
 
 }
-$data -match "root:"
+write-host "Answer 2 = $($data -match "root:")"
 
+} | select @{N="milliseconds to finish"; E={$_.TotalMilliseconds}}
